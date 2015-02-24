6e6c99d65e712ec53141892354acb18cc9ca0092
nikhil <nkmittal4994@gmail.com>
2015-02-24 23:06:55 +0530

    labels added and input,output text boxes

--- display.py
+++ display.py
@@ -1,12 +1,15 @@
 import sys
 from PyQt4 import QtCore, QtGui, uic
-
 form_class = uic.loadUiType("apergui.ui")[0]                 # Load the UI
  
 class MyWindowClass(QtGui.QMainWindow, form_class):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
-        self.setupUi(self)        
+        self.setupUi(self)
+        self.initUI()
+
+    def initUI(self):
+    	url='http://127.0.0.1:2737/translate?langpair=en|eo&q=%s'%(self.input.currentText())
 
 app = QtGui.QApplication(sys.argv)
 myWindow = MyWindowClass(None)
