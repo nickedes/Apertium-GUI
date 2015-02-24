4c8952448fc0a3e14008d1f79211a03fd9b0a288
nikhil <nkmittal4994@gmail.com>
2015-02-25 02:46:49 +0530

    translate for the given input

--- display.py
+++ display.py
@@ -1,15 +1,24 @@
 import sys
+import requests
 from PyQt4 import QtCore, QtGui, uic
 form_class = uic.loadUiType("apergui.ui")[0]                 # Load the UI
- 
+
+
 class MyWindowClass(QtGui.QMainWindow, form_class):
+
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
         self.initUI()
 
     def initUI(self):
-    	url='http://127.0.0.1:2737/translate?langpair=en|eo&q=%s'%(self.input.currentText())
+        url = 'http://127.0.0.1:2737/translate?langpair=en|eo&q=%s' % (
+            self.input.toPlainText())
+        response = requests.get(url)
+        html = response.text
+        print(html)
+        translatedString=''
+        self.output.setText(translatedString)
 
 app = QtGui.QApplication(sys.argv)
 myWindow = MyWindowClass(None)
